68f89320c810ae3d1622c1af06862e0ff8ab6b94
Jyoni <jyoni@ourchildrenoregon.org>
2021-03-29 15:51:10 -0700

    Add committ to Git repo

---
+++ Setup.R
@@ -0,0 +1,15 @@
+library(usethis)
+
+edit_git_config()
+
+create_github_token()
+
+library(gitcreds)
+
+gitcreds_set()
+
+use_git()
+
+use_github()
+
+use_github(organisation = "rin3-spring-2021", private = TRUE)
